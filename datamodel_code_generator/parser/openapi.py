3a227e9011f747e7d02b1a8743ec6a39083942a4
dexter2206 <dexter2206@gmail.com>
2020-03-20 16:08:23 +0100

    Add parsing of inline schemas (request + responses)

--- datamodel_code_generator/parser/openapi.py
+++ datamodel_code_generator/parser/openapi.py
@@ -5,6 +5,13 @@ from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
 from prance import BaseParser
 
 
+def class_from_path(path):
+    return (
+        "".join(
+            (z.title() if not z.startswith("{") else "ById") for s in path for z in s.split("-")
+        )
+    )
+
 @snooper_to_methods(max_variable_length=None)
 class OpenAPIParser(JsonSchemaParser):
     def parse_raw(self) -> None:
@@ -16,3 +23,27 @@ class OpenAPIParser(JsonSchemaParser):
             'schemas'
         ].items():  # type: str, Dict
             self.parse_raw_obj(obj_name, raw_obj)
+
+        for path, path_obj in base_parser.specification["paths"].items():
+            print(path)
+            for method, method_obj in path_obj.items():
+                if method.lower() not in ["get", "post", "put", "patch", "delete"]:
+                    continue
+                print(method)
+                for status_code, response in method_obj["responses"].items():
+                    print(status_code)
+                    if "content" not in response:
+                        continue
+                    content_type = response["content"].get("application/json", response["content"].get("text/plain"))
+                    if not content_type:
+                        continue
+                    schema = content_type["schema"]
+                    if "$ref" in schema:
+                        continue
+                    self.parse_raw_obj(class_from_path(path.split("/")) + method.title() + status_code, schema)
+                if "requestBody" not in method_obj:
+                    continue
+                schema = method_obj["requestBody"]["content"]["application/json"]["schema"]
+                if "$ref" in schema:
+                    continue
+                self.parse_raw_obj(class_from_path(path.split("/")) + method.title() + "Body", schema)
