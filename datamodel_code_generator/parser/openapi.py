deb7bc84504ab2a3763933f97f14226828eba719
awalasz <arturwalasz@gmail.com>
2020-03-25 12:15:32 +0100

    Implement generating models for query parameters defined in components.parameters

--- datamodel_code_generator/parser/openapi.py
+++ datamodel_code_generator/parser/openapi.py
@@ -6,24 +6,27 @@ from prance import BaseParser
 
 
 def class_from_path(path):
-    return (
-        "".join(
-            (z.title() if not z.startswith("{") else "ById") for s in path for z in s.split("-")
-        )
+    return "".join(
+        (z.title() if not z.startswith("{") else "ById") for s in path for z in s.split("-")
     )
 
 
 @snooper_to_methods(max_variable_length=None)
 class OpenAPIParser(JsonSchemaParser):
     def parse_raw(self) -> None:
-        base_parser = BaseParser(
-            spec_string=self.text, backend='openapi-spec-validator'
-        )
+        base_parser = BaseParser(spec_string=self.text, backend="openapi-spec-validator")
 
-        for obj_name, raw_obj in base_parser.specification['components'][
-            'schemas'
+        for obj_name, raw_obj in base_parser.specification["components"][
+            "schemas"
         ].items():  # type: str, Dict
             self.parse_raw_obj(obj_name, raw_obj)
+
+        # query parameters
+        for obj_name, raw_obj in base_parser.specification["components"].get("parameters", {}).items():
+            if raw_obj.get("in", "") == "query" and "schema" in raw_obj:
+                if "$ref" in raw_obj["schema"]:
+                    continue
+                self.parse_raw_obj(obj_name[0].upper() + obj_name[1:] + "QueryParam", raw_obj["schema"])
 
         for path, path_obj in base_parser.specification["paths"].items():
             for method, method_obj in path_obj.items():
@@ -33,13 +36,17 @@ class OpenAPIParser(JsonSchemaParser):
                 for status_code, response in method_obj["responses"].items():
                     if "content" not in response:
                         continue
-                    content_type = response["content"].get("application/json", response["content"].get("text/plain"))
+                    content_type = response["content"].get(
+                        "application/json", response["content"].get("text/plain")
+                    )
                     if not content_type:
                         continue
                     schema = content_type["schema"]
                     if "$ref" in schema:
                         continue
-                    self.parse_raw_obj(class_from_path(path.split("/")) + method.title() + status_code, schema)
+                    self.parse_raw_obj(
+                        class_from_path(path.split("/")) + method.title() + status_code, schema
+                    )
 
                 if "requestBody" not in method_obj or "$ref" in method_obj["requestBody"]:
                     continue
